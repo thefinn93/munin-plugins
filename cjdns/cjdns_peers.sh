d0727b3dd5a816664b3a776d18c7056107fef08c
thefinn93 <thefinn93@thefinn93.com>
2013-05-07 22:39:21 -0700

    Added the cjdns_peers script

---
+++ cjdns/cjdns_peers.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ "$1" = "config" ]; then
+    echo "graph_title Cjdns Peers"
+    echo "graph_info This graph shows the number of peers cjdns is connected to."
+    echo "graph_category networking"                                                                                  
+    echo "peers.label peers"
+fi
+
+if [ -z $CJDCMD ]; then
+    CJDCMD=`which cjdcmd`
+fi
+
+args="-nodns"
+
+## CJDCMD doesn't seem to actually support this, despite what the README sez
+## Follow this at https://github.com/inhies/cjdcmd/issues/36
+
+if [ "$pass" != "" ]; then
+    args="$args --pass=\"$pass\"";
+fi
+
+if [ "$host" != "" ]; then
+    args="$args --host=${host}"
+fi
+
+if [ "$port" != "" ]; then
+    args="$args --port=${port}"
+fi
+
+echo "peers.value `$CJDCMD peers $args | wc -l`"
