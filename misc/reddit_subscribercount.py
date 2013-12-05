853f21165956b0f5a78c55986932fe8a5fa8a095
Patrick Berenschot <patrick.stroef@gmail.com>
2013-12-05 21:58:06 +0100

    Created subscriber can't plugin

---
+++ misc/reddit_subscribercount.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import requests
+import os,sys
+
+try:
+    subs = os.getenv("subs", "darknetplan").split(",")
+except:
+    sys.exit(1)
+
+config = False
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "config":
+        config = True
+        
+if config:
+    print "graph_title Subreddit subscriber count"
+    print "graph_vlabel subscribers"
+    print "graph_category reddit"
+
+for sub in subs:
+    if config:
+        print sub + ".label Subscribers on /r/" + sub
+    else:
+        data = requests.get("http://www.reddit.com/r/" + sub + "/about.json")
+        print sub + ".value " + str(data.json()['data']['subscribers'])
+
