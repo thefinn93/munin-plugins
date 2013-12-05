b08372da3733e9e10dc53f1b5ee59b485100d4ab
Patrick Berenschot <patrick.stroef@gmail.com>
2013-12-05 21:47:47 +0100

    Fixes import typo and requests.get

--- misc/reddit_user.py
+++ misc/reddit_user.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python
 import sys
-impot os
+import os
  
 config = False
 if len(sys.argv) > 1:
     if sys.argv[1] == "config":
         config = True
- 
+
+user = os.getenv("username", "waaghals")
+
 if config:
-    print """graph_title Reddit Karma
-graph_info This graph shows the amount of karma that """ + os.getenv("username") + """ has.
-graph_category misc
-link.label Link Karma
-comment.label Comment Karma"""
+    print "graph_title Reddit Karma"
+    print "graph_info This graph shows the amount of karma that " + user + " has."
+    print "graph_category misc"
+    print "link.label Link Karma"
+    print "comment.label Comment Karma"
 else:
     import requests
     import json
  
-    domain = os.getenv("domain")
-    if domain == "":
-        domain = "www.reddit.com"
-    user = os.getenv("username")
- 
-    about = json.loads(requests.get("http://" + domain + "/user/" + user + "/about.json"
+    domain = os.getenv("domain", "www.reddit.com")
+
+    url = "http://" + domain + "/user/" + user + "/about.json"
+    response = requests.get(url).content
+    about = json.loads(response)
+    
     print "link.value " + str(about['data']['link_karma'])
     print "comment.value " + str(about['data']['comment_karma'])
