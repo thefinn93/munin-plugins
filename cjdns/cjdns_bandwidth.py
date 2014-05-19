b5fef69ffe600176667ff8828e501890e2b9ff87
Finn <thefinn93@thefinn93.com>
2014-05-18 18:38:23 -0700

    Labels should be IPs, not keys

--- cjdns/cjdns_bandwidth.py
+++ cjdns/cjdns_bandwidth.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import json
+from hashlib import sha512
 
 try:
     from cjdnsadmin import connect,connectWithAdminInfo
@@ -22,6 +23,63 @@ except AttributeError:
     print "Do you have an old version of cjdns?"
     print "possibly the stable-0.4 branch."
     sys.exit(1)
+
+## Stolen from contrib/python/cjdnsadmin/publicToIp6.py ##
+
+# see util/Base32.h
+def Base32_decode(input):
+    output = bytearray(len(input));
+    numForAscii = [
+        99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,
+        99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,
+        99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,
+         0, 1, 2, 3, 4, 5, 6, 7, 8, 9,99,99,99,99,99,99,
+        99,99,10,11,12,99,13,14,15,99,16,17,18,19,20,99,
+        21,22,23,24,25,26,27,28,29,30,31,99,99,99,99,99,
+        99,99,10,11,12,99,13,14,15,99,16,17,18,19,20,99,
+        21,22,23,24,25,26,27,28,29,30,31,99,99,99,99,99,
+    ];
+
+    outputIndex = 0;
+    inputIndex = 0;
+    nextByte = 0;
+    bits = 0;
+
+    while (inputIndex < len(input)):
+        o = ord(input[inputIndex]);
+        if (o & 0x80): raise ValueError;
+        b = numForAscii[o];
+        inputIndex += 1;
+        if (b > 31): raise ValueError("bad character " + input[inputIndex]);
+
+        nextByte |= (b << bits);
+        bits += 5;
+
+        if (bits >= 8):
+            output[outputIndex] = nextByte & 0xff;
+            outputIndex += 1;
+            bits -= 8;
+            nextByte >>= 8;
+
+    if (bits >= 5 or nextByte):
+        raise ValueError("bits is " + str(bits) + " and nextByte is " + str(nextByte));
+
+    return buffer(output, 0, outputIndex);
+
+
+def PublicToIp6_convert(pubKey):
+    if (pubKey[-2:] != ".k"): raise ValueError("key does not end with .k");
+    keyBytes = Base32_decode(pubKey[0:-2]);
+    hashOne = sha512(keyBytes).digest();
+    hashTwo = sha512(hashOne).hexdigest();
+    first16 = hashTwo[0:32];
+    out = '';
+    for i in range(0,8): out += first16[i*4 : i*4+4] + ":";
+    return out[:-1];
+
+
+## /theft ##
+
 
 config = False
 
@@ -68,12 +126,12 @@ for peer in peers:
         print "graph_vlabel bits"
         print "graph_category cjdns"
         for node in peers[peer]:
-          print "in_%s.label %s" % (node['publicKey'][0:10], node['publicKey'])
+          print "in_%s.label %s" % (node['publicKey'][0:10], "%s in" % PublicToIp6_convert(node['publicKey']))
           print "in_%s.type DERIVE" % node['publicKey'][0:10]
           print "in_%s.graph no" % node['publicKey'][0:10]
           print "in_%s.draw STACK" % node['publicKey'][0:10]
           print "in_%s.min 0" % node['publicKey'][0:10]
-          print "out_%s.label %s" % (node['publicKey'][0:10], node['publicKey'])
+          print "out_%s.label %s" % (node['publicKey'][0:10], "%s out" % PublicToIp6_convert(node['publicKey']))
           print "out_%s.type DERIVE" % node['publicKey'][0:10]
           print "out_%s.draw STACK" % node['publicKey'][0:10]
           print "out_%s.negative in_%s" % (node['publicKey'][0:10], node['publicKey'][0:10])
