cc8de65d146fc3177cf1a94cf974b0366cc25f46
Finn <thefinn93@thefinn93.com>
2014-05-20 16:58:14 -0700

    Show breakdown by authorizedpassword

--- cjdns/cjdns_peers.py
+++ cjdns/cjdns_peers.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     sys.path.append(os.getenv("cjdnsadmin","/opt/cjdns/contrib/python/cjdnsadmin"))
     from cjdnsadmin import connect,connectWithAdminInfo
-    
+
 if os.getenv("cjdns_password") is not None:
     cjdns = connect(os.getenv("cjdns_ip", "127.0.0.1"), int(os.getenv("cjdns_port", "11234")), os.getenv("cjdns_password"))
 else:
@@ -15,14 +15,37 @@ config = False
 if len(sys.argv) > 1:
     if sys.argv[1] == "config":
         config = True
+
+allPeers = {}
+
+i = 0
+while True:
+    ps = cjdns.InterfaceController_peerStats(i);
+    peers = ps['peers']
+    for p in peers:
+        if p['state'] != 'ESTABLISHED':
+            continue
+        name = p['publicKey'][-10:]
+        if "user" in p:
+            name = p['user']
+        if not name in allPeers:
+            allPeers[name] = 0
+        allPeers[name] =+ 1
+    if (not 'more' in ps):
+        break
+    i += 1
 if config:
-    print """graph_title Cjdns Peers
-graph_info This graph shows the number of peers cjdns is connected to.
-graph_category cjdns
-graph_vlabel peers
-peers.label peers
-peers.info Average number of peers for the last five minutes
-peers.type GAUGE
-peers.draw LINE2"""
+    print "graph_title Cjdns Peers"
+    print "graph_info This graph shows the number of peers cjdns is connected to."
+    print "graph_category cjdns"
+    print "graph_vlabel Connected Nodes"
+    for peer in allPeers:
+        print "%s.label %s" % (peer, peer)
+        print "%s.draw AREASTACK" % peer
+    print "total.label total"
+    print "total.type GAUGE"
+    print "total.draw LINE2"
 else:
-    print "peers.value %s" % cjdns.InterfaceController_peerStats()['total']
+    for peer in allPeers:
+        print "%s.value %s" % (peer, allPeers[peer])
+    print "total.value %s" % cjdns.InterfaceController_peerStats()['total']
